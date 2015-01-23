82e6ee317779203f3593d96b459ce652a6afc212
David Joerg <dsjoerg@gmail.com>
2015-01-23 10:18:01 -0500

    eval game from end backwards, massage scores so that moves are never better than perfect and equity loss is assigned to most logical place

--- djeval/__init__.py
+++ djeval/__init__.py
@@ -42,6 +42,29 @@ def score_node(engine, node):
     return (score_cp_for_white, move_object)
 
 
+# Given a list of position scores, 
+def massage_position_scores(position_scores, was_bestmove):
+
+    print "YOOOOO", position_scores
+    massaged_scores = list(position_scores)
+
+    bestmoves_plus = list(was_bestmove)
+    bestmoves_plus.append(False)
+
+    side = -1
+    for ix, score in enumerate(position_scores):
+        if ix > 0:
+            white_score_gain = score - massaged_scores[ix-1]
+            player_score_gain = side * white_score_gain
+            if bestmoves_plus[ix-1] or player_score_gain > 0:
+                massaged_scores[ix] = massaged_scores[ix-1]
+            print "%i: side %i score %i prev %i gain %i bestmove %s massaged %i" % (ix, side, score, massaged_scores[ix-1], player_score_gain, bestmoves_plus[ix-1], massaged_scores[ix])
+#        print ix, score, massaged_scores[ix]
+        side = side * -1
+
+    return massaged_scores
+
+
 # returns a dict with:
 # bcid: our 'blundercheck id' for the game
 # position_scores: a list with one integer per ply, containing the score for that ply
@@ -80,9 +103,11 @@ def do_it(engine, game=None, depth=15, debug=False):
         # TODO count loss as zero when player preserves mate count (rather than a loss of -1)
 
         if debug:
-            thismove_analysis = '%2d%-3s %6s loss:%5.0f (equity: %+5.0f) (best move: %s)' % (node.board().fullmove_number, turn_indicator, node.board().san(next_node.move), score_loss, next_score_white, node.board().san(best_move))
+            thismove_analysis = '%2d%-3s %6s loss:%5.0f (%+5.0f -> %+5.0f) (best move: %s)' % (node.board().fullmove_number, turn_indicator, node.board().san(next_node.move), score_loss, current_score_white, next_score_white, node.board().san(best_move))
             print thismove_analysis
             #        print >>outfile, thismove_analysis
+        else:
+            print '.',
 
         outstruct['position_scores'].append(next_score_white)
         outstruct['best_moves'].append(node.board().san(best_move))
@@ -92,4 +117,83 @@ def do_it(engine, game=None, depth=15, debug=False):
         best_move = next_best_move
 
     outstruct['runtime'] = time.time() - begin_time
+    print '\n'
+    return outstruct
+
+
+def do_it_backwards(engine, game=None, depth=15, debug=False):
+
+    msg("Hi! Analyzing %s BACKWARDS" % game.headers['Event'])
+
+    begin_time = time.time()
+
+    outstruct = {}
+    outstruct['event'] = game.headers['Event']
+    outstruct['position_scores'] = []
+    outstruct['best_moves'] = []
+
+    was_bestmove = []
+
+    node = game.end()
+
+    (current_score_white, best_move) = score_node(engine, node)
+
+    while node.parent:
+
+        turn_indicator = '.'
+        score_sign = 1
+        if node.board().turn == chess.WHITE:
+            turn_indicator = '...'
+            score_sign = -1
+
+        prev_node = node.parent
+        (prev_score_white, prev_best_move) = score_node(engine, prev_node)
+        score_loss = score_sign * (prev_score_white - current_score_white)
+
+        if debug:
+            thismove_analysis = '%2d%-3s %6s loss:%5.0f (%+5.0f -> %+5.0f) (best move: %s)' % (prev_node.board().fullmove_number, turn_indicator, prev_node.board().san(node.move), score_loss, prev_score_white, current_score_white, prev_node.board().san(prev_best_move))
+            print thismove_analysis
+            #        print >>outfile, thismove_analysis
+        else:
+            print '.',
+
+        outstruct['position_scores'].insert(0, current_score_white)
+        outstruct['best_moves'].insert(0, prev_node.board().san(prev_best_move))
+        was_bestmove.insert(0, prev_best_move == node.move)
+
+        node = prev_node
+        current_score_white = prev_score_white
+        best_move = prev_best_move
+
+
+    outstruct['position_scores'].insert(0, current_score_white)
+    outstruct['massaged_position_scores'] = massage_position_scores(outstruct['position_scores'], was_bestmove)
+    mps = outstruct['massaged_position_scores']
+
+    node = game
+
+    print "YO", len(mps)
+    if debug:
+        score_index = 0
+        while node.variations:
+
+            turn_indicator = '.'
+            score_sign = 1
+            if node.board().turn == chess.BLACK:
+                turn_indicator = '...'
+                score_sign = score_sign * -1
+
+            next_node = node.variation(0)
+            score_loss = score_sign * (mps[score_index] - mps[score_index + 1])
+
+            thismove_analysis = '%2d%-3s %6s loss:%5.0f (%+5.0f -> %+5.0f) (best move: %s)' % (node.board().fullmove_number, turn_indicator, node.board().san(next_node.move), score_loss, mps[score_index], mps[score_index+1], outstruct['best_moves'][score_index])
+            print thismove_analysis
+
+            node = next_node
+            score_index = score_index + 1
+
+
+#    print outstruct['position_scores']
+    outstruct['runtime'] = time.time() - begin_time
+    print '\n'
     return outstruct

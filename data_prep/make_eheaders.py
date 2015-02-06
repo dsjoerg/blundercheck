0db6dffd7fd1c0e02546b9805f7650bbf2e0bcd2
David Joerg <dsjoerg@gmail.com>
2015-02-06 12:58:22 -0500

    i thought i added you you sneaky file

---
+++ data_prep/make_eheaders.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import chess.pgn
+import cPickle as pickle
+
+def compute_maps():
+  # map from player-game to ELO
+  elos = {}
+  result = {}
+  checkmate = {}
+
+  for offset, headers in chess.pgn.scan_headers(sys.stdin):
+    if 'Event' not in headers:
+      continue
+    event_num = int(headers['Event'])
+    if 'WhiteElo' in headers:
+      elos[(event_num, 1)] = int(headers['WhiteElo'])
+    if 'BlackElo' in headers:
+      elos[(event_num, -1)] = int(headers['BlackElo'])
+    if 'Result' in headers:
+      result[event_num] = result_side[headers['Result']]
+    gamefile.seek(offset)
+    game = chess.pgn.read_game(gamefile)
+    node = game.end()
+    checkmate[event_num] = node.board().is_checkmate()
+
+  return elos, result, checkmate
+
+elos, result, checkmate = compute_maps()
+eheaders = {'elos': elos,
+            'result': result,
+            'checkmate': checkmate}
+pickle.dump(eheaders, sys.stdout)

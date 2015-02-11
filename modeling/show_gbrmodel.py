d6c0ccdfcf15e78c5700d6c19d3a551f213a83df
David Joerg <dsjoerg@gmail.com>
2015-02-10 20:10:39 -0500

    fun

--- modeling/show_gbrmodel.py
+++ modeling/show_gbrmodel.py
@@ -15,3 +15,13 @@ print DataFrame([gbr.feature_importances_, features]).transpose().sort([0], asce
 print "There are %i estimators. Here is the first one:" % len(gbr.estimators_)
 
 print gbr.estimators_[0][0].tree_
+
+dot_data = StringIO()
+tree.export_graphviz(gbr.estimators_[0][0], out_file=dot_data, feature_names=features)
+print dot_data.getvalue()
+
+B=pgv.AGraph(dot_data.getvalue())
+B.layout('dot')
+B.draw('/data/gbr.png') # draw png
+
+print "Wrote that estimator to /data/gbr.png"

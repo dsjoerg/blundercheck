9f877f4fab378a388e60eef89688ffcc89569f04
David Joerg <dsjoerg@gmail.com>
2015-02-11 16:48:02 -0500

    getting ready to process latest server run

---
+++ combine/contest_20150211a/data_prep/make_depth_aggregates_csv.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import sys, csv
+import numpy as np
+
+depths = []
+seldepths = []
+depths_agreeing_ratio = {}
+deepest_agree_ratio = {}
+depths_agreeing_ratio[1] = []
+depths_agreeing_ratio[-1] = []
+deepest_agree_ratio[1] = []
+deepest_agree_ratio[-1] = []
+
+def movenum_to_side(movenum):
+    if movenum % 2 == 0:
+        return 1
+    else:
+        return -1
+
+def dump_rows():
+    global depths
+    global seldepths
+    global depths_agreeing_ratio
+    global deepest_agree_ratio
+
+    if current_game == 0:
+        return
+    for side in [1, -1]:
+        print current_game,
+        print side,
+        print np.mean(depths),
+        print np.mean(seldepths),
+        print np.mean(depths_agreeing_ratio[side]),
+        print np.mean(deepest_agree_ratio[side]),
+        if len(depths_agreeing_ratio[side]) == 0:
+            print 0.5,
+        else:
+            print float(np.count_nonzero(depths_agreeing_ratio[side])) / len(depths_agreeing_ratio[side]),
+        print len(depths)
+    depths = []
+    seldepths = []
+    depths_agreeing_ratio[1] = []
+    depths_agreeing_ratio[-1] = []
+    deepest_agree_ratio[1] = []
+    deepest_agree_ratio[-1] = []
+
+columns = [
+'halfply',
+'moverscore',
+'movergain',
+'move_piece',
+'move_dir',
+'move_dist',
+'move_is_capture',
+'move_is_check',
+'bestmove_piece',
+'bestmove_dir',
+'bestmove_dist',
+'bestmove_is_capture',
+'bestmove_is_check',
+'depth',
+'seldepth',
+'depths_agreeing',
+'deepest_agree',
+'elo',
+'side',
+'gamenum',
+]
+
+depths = []
+seldepths = []
+depths_agreeing_ratio = {}
+depths_agreeing_ratio[1] = []
+depths_agreeing_ratio[-1] = []
+deepest_agree_ratio = {}
+deepest_agree_ratio[1] = []
+deepest_agree_ratio[-1] = []
+
+csvreader = csv.DictReader(sys.stdin, fieldnames=columns)
+
+
+current_game = 0
+rownum = 0
+
+for row in csvreader:
+    if row['gamenum'] != current_game:
+        dump_rows()
+        current_game = row['gamenum']
+    depths.append(int(row['depth']))
+    seldepths.append(int(row['seldepth']))
+    side = int(row['side'])
+    depths_agreeing_ratio[side].append(float(row['depths_agreeing']) / float(row['depth']))
+    deepest_agree_ratio[side].append(float(row['deepest_agree']) / float(row['depth']))
+    rownum = rownum + 1
+#    if rownum > 10000:
+#        break
---
+++ combine/contest_20150211a/data_prep/make_scatterplots.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import matplotlib
+matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
+
+import sys
+import seaborn as sns
+from pandas import read_pickle, qcut
+from itertools import combinations
+import matplotlib.pyplot as plt
+from djeval import *
+
+sns.set_palette("deep", desat=.6)
+sns.set_context(rc={"figure.figsize": (8, 4)})
+
+msg("Hello there, reading yy_df.")
+yy_df = read_pickle(sys.argv[1])
+
+x = yy_df['nmerror']
+y = yy_df['elo']
+with sns.axes_style("white"):
+    sns.jointplot(x, y, kind="hex")
+plt.savefig('/data/seaborn.png')
+plt.close()
+
+with_elo = yy_df[yy_df['elo'].notnull()]
+
+features = ['nmerror',
+            'blunderrate', 'noblunders', 
+            'perfectrate',
+            'gameoutcome',
+            'won_by_checkmate', 'lost_by_checkmate', 'ended_by_checkmate',
+            'my_final_equity', 'final_equity',
+            'grit', 'any_grit', 'opponent_any_grit', 'major_grit',
+            'mate_created', 'mate_destroyed', 'premature_quit',
+            'side',
+            'drawn_game',
+            'gamelength',
+            'meanecho',
+            'opponent_nmerror', 'opponent_noblunders',
+            'mean_depth_clipped',
+            'mean_seldepth',
+            'min_nmerror', 'max_nmerror', 'max_meanecho',
+            'early_lead',
+            'q_error_one', 'q_error_two',
+            'opponent_q_error_one', 'opponent_q_error_two',
+            'pct_sanemoves',
+            'opponent_blunderrate', 'opponent_perfectrate',
+            'opponent_grit', 'opponent_meanecho',
+            'opponent_mate_created', 'opponent_mate_destroyed',
+            'mean_seldepth',
+            'mean_depths_ar', 'mean_deepest_ar',
+            'opponent_mean_depths_ar', 'opponent_mean_deepest_ar',
+            'pct_sanemoves',
+            'moveelo_weighted'
+           ]
+
+plottables = ['elo', 'gbr_prediction', 'gbr_error']
+plottables.extend(['gamelength', 'mean_depth_clipped', 'mean_deepest_ar', 'opponent_mean_deepest_ar'])
+
+do_indivs = False
+if do_indivs:
+    for a, b in combinations(plottables, 2):
+        for first, second in [(a,b), (b,a)]:
+            try:
+                groupings, bins = qcut(with_elo[first], 10, labels=False, retbins=True)
+                sns.violinplot(with_elo[second], groupings)
+                plt.savefig('/data/' + first + '_' + second + '.png')
+                plt.close()
+            except:
+                print("Couldnt manage for %s %s" % (first, second))
+
+    #        f, ax = plt.subplots(figsize=(11, 6))
+    #        sns.violinplot(with_elo[second], groupings, names=[str(b) + str(b+1) for b in bins[:-1]])
+    #        ax.set(ylim=(-.7, 1.05))
+    #        sns.despine(left=True, bottom=True)
+            print '.',
+            sys.stdout.flush()
+
+
+g = sns.pairplot(with_elo[plottables], size=2.5)
+plt.savefig('/data/pairplot.png')
+plt.close()

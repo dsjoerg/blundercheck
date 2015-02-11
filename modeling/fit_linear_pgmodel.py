9ddc5ddcfaabf65609b881f4e40c332a1e12382e
David Joerg <dsjoerg@gmail.com>
2015-02-10 21:56:55 -0500

    fun

--- modeling/fit_linear_pgmodel.py
+++ modeling/fit_linear_pgmodel.py
@@ -19,6 +19,9 @@ msg("Getting subset ready.")
 # TODO save the dummies along with yy_df
 dummies = get_dummies(yy_df['opening_feature'])
 
+# TODO save the moveelo_features along with yy_df
+moveelo_features = [("moveelo_" + x) for x in ['mean', 'median', '25', '10', 'min', 'max', 'stdev']]
+
 train = yy_df[yy_df.meanerror.notnull() & yy_df.elo.notnull()]
 
 formula_rhs = "side + nmerror + gameoutcome + drawn_game + gamelength + meanecho"
@@ -31,11 +34,7 @@ formula_rhs = formula_rhs + " + mean_depths_ar + mean_deepest_ar"
 formula_rhs = formula_rhs + " + opponent_mean_depths_ar + opponent_mean_deepest_ar"
 formula_rhs = formula_rhs + " + pct_sanemoves"
 formula_rhs = formula_rhs + " + " + " + ".join(dummies.columns.values)
-
-#formula_rhs = formula_rhs + " + moveelo_25 + moveelo_mean + moveelo_10"
-#formula_rhs = formula_rhs + " + moveelo_min"
-#formula_rhs = formula_rhs + " + moveelo_stdev"
-formula_rhs = formula_rhs + " + " + " + ".join([("moveelo_" + x) for x in move_aggs.columns.values.tolist()])
+formula_rhs = formula_rhs + " + " + " + ".join(moveelo_features)
 
 formula = "elo ~ " + formula_rhs
 

58b7b92ecb97eb62f5991b6f9bd4374cac203860
David Joerg <dsjoerg@gmail.com>
2015-02-11 21:28:51 -0500

    fun

--- modeling/show_gbrmodel.py
+++ modeling/show_gbrmodel.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
+import matplotlib
+matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
+
 import sys
 import pygraphviz as pgv
 from StringIO import StringIO
 from pandas import DataFrame
 from sklearn.externals import joblib
 from sklearn import tree
+import matplotlib.pyplot as plt
 
 gbr, features = joblib.load(sys.argv[1])
+
+print "Train score:"
+print gbr.train_score_
 
 print "Feature importances:"
 print DataFrame([gbr.feature_importances_, features]).transpose().sort([0], ascending=False)

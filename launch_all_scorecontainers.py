4c334436a34bdfca692b56ba28b1bef4b6ac8328
David Joerg <dsjoerg@gmail.com>
2015-03-12 13:01:52 -0400

    trying to see why the big games aren't processing in the big run

--- launch_all_scorecontainers.py
+++ launch_all_scorecontainers.py
@@ -14,6 +14,8 @@ SCORESERVERS_PER_CONTAINER = 32
 
 envvars = []
 envvars.append({"key": "NUM_SCORESERVERS", "value": str(SCORESERVERS_PER_CONTAINER)})
+envvars.append({"key": "DEBUG", "value": "1"})
+envvars.append({"key": "MIN_ITEMS_PER_KEY", "value": "2000"})
 envvars.append({"key": "MOVETIME", "value": "16000"})
 envvars.append({"key": "THREADS", "value": "1"})
 envvars.append({"key": "HASH", "value": "1000"})

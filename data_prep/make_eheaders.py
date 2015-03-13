b8edfe1479f2225ceaa52559007e2770a9f5987d
David Joerg <dsjoerg@gmail.com>
2015-03-13 11:40:49 -0400

    fun

--- data_prep/make_eheaders.py
+++ data_prep/make_eheaders.py
@@ -37,7 +37,7 @@ def compute_maps():
       continue
     event_num = int(headers['Event'])
     if (event_num % 1000) == 0:
-      print 'make_eheaders reading game #%i' % event_num
+      sys.stderr.write('make_eheaders reading game #%i\n' % event_num)
     if 'WhiteElo' in headers:
       elos[(event_num, 1)] = int(headers['WhiteElo'])
     if 'BlackElo' in headers:
